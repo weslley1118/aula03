b5fae551f40a33dee03a0cd41f34da547a77fabd
weslley1118 <nakamurahenrique31@gmail.com>
2024-11-02 20:16:58 -0400

    é nois

--- src/pages/abu.jsx
+++ src/pages/abu.jsx
@@ -2,18 +2,22 @@ export default function Lista({produtos}){
     return (
         <>
           <h1>Lista de Produtos</h1>
+            <div className="Dog">
           <ul className="produtosgerais">
             {produtos.map(produto => (
+              <div className="Gat">
               <li className="nome" key={produto.id}>
                 <div className="sel">
                 <h2 >{produto.title}</h2>
                 <p >{produto.description}</p>
                 <p>Preço: ${produto.price}</p>
-                <img src={produto.image} alt={produto.title} width={100} />
+                <img className="imagen" src={produto.image} alt={produto.title} width={100} />
                 </div>
               </li>
+              </div>
             ))}
           </ul>
+            </div>
         </>
       )
     }
